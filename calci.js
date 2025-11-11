c64b2733e8c2031028b4d7b4e3374dc756f1ad25
PrinceTiwari18 <princetiwari4554@gmail.com>
2025-11-11 11:43:07 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("input");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "AC") {
+            input.value = "";
+        } else if (value === "C") {
+            input.value = input.value.slice(0, -1);
+        } else if (value === "=") {
+            try {
+                input.value = eval(input.value);
+            } catch (error) {
+                input.value = "Error";
+            }
+        } else {
+            input.value += value;
+        }
+    });
+});
